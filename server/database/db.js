1b55442b0998c1bb152fc896b2cc481e455c7659
Danishsjjd <danishsjjad@gmail.com>
2022-10-02 15:49:05 +0500

    remove debug, improve sendMail

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-const debug = require("debug")("app:db");
 
 module.exports = function connection() {
-	mongoose
-		.connect(process.env.DATABASE_URL)
-		.then((data) => {
-			debug(`mongodb is connected with server: ${data.connection.host}`);
-		})
-		.catch((err) => debug(err));
+  mongoose
+    .connect(process.env.DATABASE_URL)
+    .then((data) => {
+      console.log(`mongodb is connected with server: ${data.connection.host}`);
+    })
+    .catch((err) => debug(err));
 };
